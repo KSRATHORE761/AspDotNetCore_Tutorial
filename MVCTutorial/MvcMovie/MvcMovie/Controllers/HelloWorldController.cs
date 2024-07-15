ddefb3c0c00fa29359c073d9b427c00cdc746e09
KSRATHORE761 <rathore.kuldeep761@gmail.com>
2024-07-15 23:26:19 +0530

    MVC

---
+++ MVCTutorial/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Get : /HelloWorld
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Get : /HelloWorld/Welcome
+        public IActionResult Welcome(string name, int numtimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["numTimes"] = numtimes;
+            return View();
+        }
+    }
+}

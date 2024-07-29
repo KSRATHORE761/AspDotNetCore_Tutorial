580d769da6816598ac675faaf29611559a0713d7
KSRATHORE761 <rathore.kuldeep761@gmail.com>
2024-07-29 06:34:54 +0530

    CustomMiddleware

---
+++ CustomMiddlewareApp/CustomMiddlewareApp/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CustomMiddlewareApp.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Console.WriteLine("An exception occurred: " + ex.Message);
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
---
+++ CustomMiddlewareApp/CustomMiddlewareApp/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CustomMiddlewareApp.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+
+    public class LoggingMiddleware
+    {
+        private RequestDelegate _next;
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var startTime = DateTime.Now;
+            Console.WriteLine($"starttime:{startTime.Millisecond}");
+            await _next(httpContext);
+            var endTime = DateTime.Now;
+            Console.WriteLine($"endTime:{endTime.Millisecond}");
+            var elapsedTime = endTime - startTime;
+            var logMessage = $"{httpContext.Request.Method} {httpContext.Request.Path} {httpContext.Response.StatusCode} {elapsedTime.TotalMilliseconds}ms";
+            Console.WriteLine(logMessage);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class LoggingMiddlewareExtension
+    {
+        public static IApplicationBuilder UseLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+
+}

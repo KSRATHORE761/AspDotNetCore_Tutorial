0d0b06aa2f042d71644d500e41d830b5276ca1f1
KSRATHORE761 <rathore.kuldeep761@gmail.com>
2024-09-22 23:41:55 +0530

    Interface Segeration Principle

---
+++ SOLID/InterfaceSegregationPrinciple/InterfaceSegregationPrinciple/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+
+    //ISP violation: One interface with too many responsiblities
+    public interface IWorker
+    {
+        void work();
+        void Eat();
+    }
+    //Correcting ISP by splitting into two interfaces
+    public interface IWorkable
+    {
+        void work();
+    }
+    public interface IFeedable
+    {
+        void Eat();
+    }
+
+    //Implementing only what is need
+    public class Worker:IWorkable,IFeedable
+    {
+        public void work() => Console.WriteLine("Working...");
+        public void Eat() => Console.WriteLine("Eating...");
+    }
+    public class Robot : IWorkable
+    {
+        public void work() => Console.WriteLine("Robot Working...");
+        //Robot doesn't need to implement Eat method
+    }
+}
